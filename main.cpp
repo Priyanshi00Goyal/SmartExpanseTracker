c7751d835a25dbca3a337a1a1577011ddb1faa93
Priyanshi Goyal <p.25012008.g@gmail.com>
2026-07-13 12:13:07 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,46 @@
+#include <iostream>
+#include "User.h"
 
+using namespace std;
+
+int main()
+{
+    User user;
+    int choice;
+
+    do
+    {
+        cout << "\n=================================\n";
+        cout << "    SMART EXPENSE TRACKER\n";
+        cout << "=================================\n";
+        cout << "1. Register\n";
+        cout << "2. Login\n";
+        cout << "3. Exit\n";
+        cout << "Enter your choice: ";
+        cin >> choice;
+
+        switch(choice)
+        {
+            case 1:
+                user.registerUser();
+                break;
+
+            case 2:
+                if(user.login())
+                {
+                    cout << "\nWelcome to Smart Expense Tracker!\n";
+                }
+                break;
+
+            case 3:
+                cout << "\nThank you for using Smart Expense Tracker!\n";
+                break;
+
+            default:
+                cout << "\nInvalid Choice! Please try again.\n";
+        }
+
+    } while(choice != 3);
+
+    return 0;
+}
