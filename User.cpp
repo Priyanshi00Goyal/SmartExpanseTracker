3ea4bb4bad241e81e21b96e7bb9356e9487609c4
Priyanshi Goyal <p.25012008.g@gmail.com>
2026-07-13 12:05:21 +0530

    Add user registration functionality

--- User.cpp
+++ User.cpp
@@ -3,3 +3,22 @@
 #include <fstream>
 
 using namespace std;
+
+void User::registerUser()
+{
+    cout << "\n===== User Registration =====\n";
+
+    cout << "Enter Username: ";
+    cin >> username;
+
+    cout << "Enter Password: ";
+    cin >> password;
+
+    ofstream file("data/users.txt", ios::app);
+
+    file << username << " " << password << endl;
+
+    file.close();
+
+    cout << "\nRegistration Successful!\n";
+}
