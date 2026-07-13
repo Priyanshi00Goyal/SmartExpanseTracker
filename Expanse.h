c07add50c34268905198c03da991869ee293fc0a
Priyanshi Goyal <p.25012008.g@gmail.com>
2026-07-13 15:27:31 +0530

    Add Expense class definition in Expanse.h

--- Expanse.h
+++ Expanse.h
@@ -1,1 +1,17 @@
+#ifndef EXPENSE_H
+#define EXPENSE_H
 
+#include <string>
+
+using namespace std;
+
+class Expense
+{
+public:
+    string title;
+    double amount;
+    string date;
+    string category;
+};
+
+#endif 
