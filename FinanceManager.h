787f2553b94e3c5970d07a5e1bc99102ed8a51f8
Priyanshi Goyal <p.25012008.g@gmail.com>
2026-07-13 16:34:21 +0530

    Fix include statements and clean up Income class

--- FinanceManager.h
+++ FinanceManager.h
@@ -1,32 +1,33 @@
 #ifndef FINANCEMANAGER_H
 #define FINANCEMANAGER_H
-#include<vector>
+
+#include <vector>
+
+#include "Income.h"
 #include "Expense.h"
+
+using namespace std;
 
 class FinanceManager
 {
 private:
     vector<Income> incomes;
-    vector<Expense> expenses;
+    vector<Expense> expenses; 
     double monthlyBudget = 0;
 
 public:
     void dashboard();
+
     void addIncome();
     void viewIncome();
+
     void addExpense();
     void viewExpense();
+
     void monthlyReport();
+
     void setBudget();
     void checkBudget();
-};
-
-class Income
-{
-public:
-    string source;
-    double amount;
-    string date;
 };
 
 #endif
