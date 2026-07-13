4d7d621277151c78213581cf072b0f863d0ce660
Priyanshi Goyal <p.25012008.g@gmail.com>
2026-07-13 15:27:06 +0530

    Add Expense management to FinanceManager class

--- FinanceManager.h
+++ FinanceManager.h
@@ -1,15 +1,24 @@
 #ifndef FINANCEMANAGER_H
 #define FINANCEMANAGER_H
 #include<vector>
+#include "Expense.h"
 
 class FinanceManager
 {
 private:
     vector<Income> incomes;
+    vector<Expense> expenses;
+    double monthlyBudget = 0;
 
 public:
     void dashboard();
     void addIncome();
+    void viewIncome();
+    void addExpense();
+    void viewExpense();
+    void monthlyReport();
+    void setBudget();
+    void checkBudget();
 };
 
 class Income
