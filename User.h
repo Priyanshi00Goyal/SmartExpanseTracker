12a7805a84f265263c2ed4e59e3e119ae58a6d48
Priyanshi Goyal <p.25012008.g@gmail.com>
2026-07-13 12:01:42 +0530

    Update User.h

--- User.h
+++ User.h
@@ -1,1 +1,19 @@
+#ifndef USER_H
+#define USER_H
 
+#include <string>
+
+using namespace std;
+
+class User
+{
+private:
+    string username;
+    string password;
+
+public:
+    void registerUser();
+    bool login();
+};
+
+#endif
